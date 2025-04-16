350dcc42cafc8725e65722b23b6371bd925b9b67
Jakub <kwasik15@gmail.com>
2025-04-16 23:24:25 +0200

    API returns db // new model

--- backend/routes.py
+++ backend/routes.py
@@ -1,9 +1,28 @@
 from flask import Blueprint, jsonify
+from sqlalchemy import create_engine, text
+import pymysql
 
 api = Blueprint('api', __name__)
 
+# Database connection details
+username = 'michals2'
+password = 'Nv1VZRmuTxRvJn3u'
+host = 'mysql.agh.edu.pl'
+port = 3306
+database = 'michals2'
+
+connection_string = f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'
+
+engine = create_engine(connection_string)
+
 @api.route('/api/perfumes', methods=['GET'])
 def get_perfumes():
-    # Implement logic to fetch and return perfume data
-    perfumes = [...]  # Retrieve perfumes from database or other source
+
+    query = text("SELECT * FROM Perfumes")  
+    with engine.connect() as connection:
+        result = connection.execute(query)
+        perfumes = [dict(row._mapping) for row in result]
+    
+    print(perfumes) 
+
     return jsonify(perfumes)
