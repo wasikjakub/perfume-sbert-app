36d6a245ac5f01baa9a25311047814c0fedba285
Jakub <kwasik15@gmail.com>
2025-05-16 20:55:49 +0200

    new logo + families pgae

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/shared/Header';
 import Home from './pages/Home';
+import Families from './pages/Families';
 
 function App() {
   return (
-    <div className="bg-[#f6e9e6] min-h-screen">
-      <Header />
-      <main className="p-6 pb-40">
-        <Home />
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="bg-[#f6e9e6] min-h-screen">
+        <Header />
+        <main className="p-6 pb-40">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/families" element={<Families />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
